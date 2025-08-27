e4b37f69e13b2c3c5199ccc15006d1c247f18145
sik2 <bickwang123@gmail.com>
2025-08-27 09:34:09 +0900

    init

---
+++ src/main/java/com/back/P145171Application.java
@@ -0,0 +1,13 @@
+package com.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P145171Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(P145171Application.class, args);
+    }
+
+}
