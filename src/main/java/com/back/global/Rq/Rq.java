cf2968c5cae87ba2bafa908e2235b94f9eb10899
sik2 <bickwang123@gmail.com>
2025-09-01 10:33:49 +0900

    session scope

--- src/main/java/com/back/global/Rq/Rq.java
+++ src/main/java/com/back/global/Rq/Rq.java
@@ -2,8 +2,10 @@ package com.back.global.Rq;
 
 import lombok.Getter;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.SessionScope;
 
 @Component
+@SessionScope
 public class Rq {
     @Getter
     private int count;
