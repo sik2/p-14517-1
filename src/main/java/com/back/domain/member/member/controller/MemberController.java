37573c364953c9a2092fc8bd079e3b88255349cb
sik2 <bickwang123@gmail.com>
2025-09-01 10:45:13 +0900

    session scope login

--- src/main/java/com/back/domain/member/member/controller/MemberController.java
+++ src/main/java/com/back/domain/member/member/controller/MemberController.java
@@ -2,6 +2,7 @@ package com.back.domain.member.member.controller;
 
 import com.back.domain.member.member.dto.Member;
 import com.back.domain.member.member.service.MemberService;
+import com.back.global.Rq.Rq;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class MemberController {
     private final MemberService memberService;
+    private final Rq rq;
 
     @GetMapping("/login")
     public  String showLogin(){
@@ -36,6 +38,7 @@ public class MemberController {
             return "비밀번호가 일치 하지 않습니다.";
         }
 
+        rq.setName(member.getName());
         session.setAttribute("loginedMemerId", member.getId());
 
         return "로그인 처리";
@@ -44,6 +47,7 @@ public class MemberController {
     @GetMapping("/logout")
     @ResponseBody
     public String logout(HttpSession session) {
+        rq.setName("로그아웃됨");
         session.removeAttribute("loginedMemerId");
 
         return "로그아웃 처리";
--- src/main/java/com/back/global/Rq/Rq.java
+++ src/main/java/com/back/global/Rq/Rq.java
@@ -1,6 +1,7 @@
 package com.back.global.Rq;
 
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.annotation.SessionScope;
 
@@ -9,6 +10,10 @@ import org.springframework.web.context.annotation.SessionScope;
 public class Rq {
     @Getter
     private int count;
+
+    @Getter
+    @Setter
+    private String name;
 
     public int increaseCount() {
         return count++;
